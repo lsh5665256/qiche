8fc29310e44c7b3d8f8b2537caacb028b84603d8
lsh5665256 <490830076@qq.com>
2019-12-03 17:08:12 +0800

    rem

--- qiche-ziji/chexi/src/main.js
+++ qiche-ziji/chexi/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import VueLazyload from 'vue-lazyload'
+import "lib-flexible/flexible.js"
 Vue.use(VueLazyload)
 import axios from "axios"
 Vue.prototype.axios=axios
