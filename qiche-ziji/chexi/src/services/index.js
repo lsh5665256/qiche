0fa4cd4a53115cc55e5ce5e7d8c257764dce77e8
lsh5665256 <490830076@qq.com>
2019-12-07 00:39:59 +0800

    sdas

--- qiche-ziji/chexi/src/services/index.js
+++
@@ -1,3 +0,0 @@
-export * from './modules/home'
-export * from './modules/Zxdj'
-export * from "./modules/city"
--- qiche-ziji/chexi/src/services/modules/city.js
+++
@@ -1,6 +0,0 @@
-import request from '@/utils/request';
-
-// 获取城市数据，既axios拿回来的数据
-export let nacity = ()=>{
-    return request('/location-client.html')
-}
